e5041df990cf3d77694942b8aa30bf02e3029a50
mwhyte <mrmwhyte@gmail.com>
2019-07-16 23:14:14 +0100

    - remember me cookie based implementation - formatting

--- src/main/java/com/codenerve/spring/sec/config/WebSecurityConfig.java
+++ src/main/java/com/codenerve/spring/sec/config/WebSecurityConfig.java
@@ -12,43 +12,41 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private AuthenticationSuccessHandler authenticationSuccessHandler;
-
-    @Autowired
-    public WebSecurityConfig(AuthenticationSuccessHandler authenticationSuccessHandler) {
-        this.authenticationSuccessHandler = authenticationSuccessHandler;
-    }
-
+    // @formatter:off
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
+            .authorizeRequests()
                 .antMatchers( "/css/**").permitAll()
-                .antMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated()
-                .and()
-                .formLogin()
+
+            .and()
+            .formLogin()
                 .loginPage("/login")
-                .successHandler(authenticationSuccessHandler)
+                .defaultSuccessUrl("/index")
                 .permitAll()
-                .and()
+
+            .and()
                 .logout()
                 .permitAll()
-                .and().csrf().disable(); // we'll enable this in a later blog post
+
+            .and()
+                .rememberMe()
+
+            .and()
+                .csrf().disable(); // we'll enable this in a later blog post
     }
+    // @formatter:on
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("user").password("{noop}pass").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}pass").roles("ADMIN");
+                .withUser("user").password("{noop}pass").roles("USER");
     }
 }
