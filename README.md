38caaac3dc5198c6a2f21fd85d36796b9ee5eb3f
Michael Whyte <mwhyte@apexclearing.com>
2021-12-08 19:51:18 +0000

    updating libs and copyright

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Lessons at [codenerve.com](https://www.codenerve.com)
 
 Repo contents by branch:
 
-| Lesson   |     Branch   |    
-|:--------:|:-------------|
-| [1 - Basic form login](https://www.codenerve.com/spring-security-basic-login-form/)                       | [branch](https://github.com/MWhyte/spring-security-basic/tree/1.basic-form-login)| 
-| [2 - Thymeleaf extras](https://www.codenerve.com/spring-security-thymeleaf-extras/)                       | [branch](https://github.com/MWhyte/spring-security-basic/tree/2.thymeleaf-extras)|
-| [3 - Redirect based on user roles](https://www.codenerve.com/spring-security-user-role-redirect/)         | [branch](https://github.com/MWhyte/spring-security-basic/tree/3.redirect-based-on-role)|
+| Lesson                                                                                                   |     Branch   |    
+|:---------------------------------------------------------------------------------------------------------|:-------------|
+| [1 - Basic form login](https://www.codenerve.com/spring-security-basic-login-form/)                      | [branch](https://github.com/MWhyte/spring-security-basic/tree/1.basic-form-login)| 
+| [2 - Thymeleaf extras](https://www.codenerve.com/spring-security-thymeleaf-extras/)                      | [branch](https://github.com/MWhyte/spring-security-basic/tree/2.thymeleaf-extras)|
+| [3 - Redirect based on user roles](https://www.codenerve.com/spring-security-user-role-redirect/)        | [branch](https://github.com/MWhyte/spring-security-basic/tree/3.redirect-based-on-role)|
--- src/test/java/com/codenerve/spring/sec/ApplicationTests.java
+++ src/test/java/com/codenerve/spring/sec/ApplicationTests.java
@@ -1,20 +1,11 @@
 /*
- * Copyright (c) 2018 codenerve.com
+ * Copyright (c) 2022 mwhyte.dev
  *
  * You may study, use, and modify this example. Redistribution is not permitted.
  */
 
 package com.codenerve.spring.sec;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.FormLoginRequestBuilder;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
@@ -23,51 +14,61 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.FormLoginRequestBuilder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ApplicationTests {
 
-    @Autowired
-    private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
-    @Test
-    public void loginWithValidUserThenAuthenticated() throws Exception {
-        FormLoginRequestBuilder login = formLogin()
-                .user("user")
-                .password("pass");
+  @Test
+  public void loginWithValidUserThenAuthenticated() throws Exception {
+    FormLoginRequestBuilder login = formLogin()
+        .user("user")
+        .password("pass");
 
-        mockMvc.perform(login)
-                .andExpect(authenticated().withUsername("user"));
-    }
+    mockMvc.perform(login)
+        .andExpect(authenticated().withUsername("user"));
+  }
 
-    @Test
-    public void loginWithInvalidUserThenUnauthenticated() throws Exception {
-        FormLoginRequestBuilder login = formLogin()
-                .user("invalid")
-                .password("invalidpassword");
+  @Test
+  public void loginWithInvalidUserThenUnauthenticated() throws Exception {
+    FormLoginRequestBuilder login = formLogin()
+        .user("invalid")
+        .password("invalidpassword");
 
-        mockMvc.perform(login)
-                .andExpect(unauthenticated());
-    }
+    mockMvc.perform(login)
+        .andExpect(unauthenticated());
+  }
 
-    @Test
-    public void accessUnsecuredResourceThenOk() throws Exception {
-        mockMvc.perform(get("/css/style.css"))
-                .andExpect(status().isOk());
-    }
+  @Test
+  public void accessUnsecuredResourceThenOk() throws Exception {
+    mockMvc.perform(get("/css/style.css"))
+        .andExpect(status().isOk());
+  }
 
-    @Test
-    public void accessSecuredResourceUnauthenticatedThenRedirectsToLogin() throws Exception {
-        mockMvc.perform(get("/hello"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrlPattern("**/login"));
-    }
+  @Test
+  public void accessSecuredResourceUnauthenticatedThenRedirectsToLogin() throws Exception {
+    mockMvc.perform(get("/hello"))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(redirectedUrlPattern("**/login"));
+  }
 
-    @Test
-    @WithMockUser
-    public void accessSecuredResourceAuthenticatedThenOk() throws Exception {
-        mockMvc.perform(get("/index"))
-                .andExpect(status().isOk());
-    }
+  @Test
+  @WithMockUser
+  public void accessSecuredResourceAuthenticatedThenOk() throws Exception {
+    mockMvc.perform(get("/index"))
+        .andExpect(status().isOk());
+  }
 }
