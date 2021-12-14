5fdf6328e9e2c7cacbd5d40efcfd40d8249139c4
Michael Whyte <mwhyte@apexclearing.com>
2021-12-14 20:09:42 +0000

    formatting

--- src/main/java/dev/mwhyte/spring/sec/config/WebSecurityConfig.java
+++ src/main/java/dev/mwhyte/spring/sec/config/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/css/**").permitAll()
+                .antMatchers( "/css/**", "/images/**", "/favicon.ico").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
